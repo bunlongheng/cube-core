52880256e5e997f791485ddbe713c50a1ee9cff2
Bunlong Heng <bheng.code@gmail.com>
2025-06-10 17:40:47 -0400

    tried a few more options on docker-compose;

--- start.sh
+++ start.sh
@@ -1,1 +1,19 @@
-docker-compose up --build
+docker-compose up -d --build
+
+docker-compose logs -f cube_api
+
+docker exec -it cube-core-cube_api-1 sh
+apt update && apt install curl -y
+curl http://cubestore_router:3030
+
+
+docker ps | grep cubestore_router
+
+
+docker exec -it cube-core-cubestore_router-1 bash
+ps aux | grep cubestored
+
+
+docker-compose down --volumes --remove-orphans
+docker-compose up -d --build
+docker-compose logs -f cubestore_router
